ed911ef011eefb7d33398d54a1aae5eaff6837e4
acarrasquillo <abimael.carrasquillo@gmail.com>
2015-02-05 14:57:36 -0400

    Bootstatrped

--- index.php
+++ index.php
@@ -1,33 +1,66 @@
-<!DOCTYPE hmtl>
-<cfheader name="X-XSS-Protection" value="0">
-<html>
-<head>
-<title> Simple XSS Exercice</title>
-</head>
-<body>
-	<?php
-		header("X-XSS-Protection: 0");
-	?>
+  <?php include('header.php'); ?>
+  <?php include('navbar.php'); ?>
 
-	<form name="XSS" action="index.php" method="post">
-		Search :<input type="text" name="input" value="<?php print $input;?>">
-		<input type="submit" value="submit">
-	</form>
+      <div class="row row-offcanvas row-offcanvas-right">
 
-	<p>  
+        <div class="col-xs-12 col-sm-9">
+          <p class="pull-right visible-xs">
+            <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle nav</button>
+          </p>
+          <div class="jumbotron">
+            <h1>Cybersecurity</h1>
+            <p>This is an example to show the potential of an offcanvas layout pattern in Bootstrap. Try some responsive-range viewport sizes to see it in action.</p>
+          </div>
+          <div class="row">
+            <div class="col-xs-6 col-lg-4">
+              <h2>Heading</h2>
+              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+              <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+            </div><!--/.col-xs-6.col-lg-4-->
+            <div class="col-xs-6 col-lg-4">
+              <h2>Heading</h2>
+              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+              <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+            </div><!--/.col-xs-6.col-lg-4-->
+            <div class="col-xs-6 col-lg-4">
+              <h2>Heading</h2>
+              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+              <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+            </div><!--/.col-xs-6.col-lg-4-->
+            <div class="col-xs-6 col-lg-4">
+              <h2>Heading</h2>
+              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+              <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+            </div><!--/.col-xs-6.col-lg-4-->
+            <div class="col-xs-6 col-lg-4">
+              <h2>Heading</h2>
+              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+              <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+            </div><!--/.col-xs-6.col-lg-4-->
+            <div class="col-xs-6 col-lg-4">
+              <h2>Heading</h2>
+              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+              <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+            </div><!--/.col-xs-6.col-lg-4-->
+          </div><!--/row-->
+        </div><!--/.col-xs-12.col-sm-9-->
 
-		<?php 
-			if ($_POST["input"]== ""){
-				
-			}
-			else{
-				$texto = $_POST["input"];
-				print $texto;
-			}	
-					
+        <div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar">
+          <div class="list-group">
+            <a href="#" class="list-group-item active">Lab 1</a>
+            <a href="#" class="list-group-item">Lab 2</a>
+            <a href="#" class="list-group-item">Lab 3</a>
+            <a href="#" class="list-group-item">Lab 4</a>
+            <a href="#" class="list-group-item">Lab 5</a>
+            <a href="#" class="list-group-item">Lab 6</a>
+            <a href="#" class="list-group-item">Lab 7</a>
+            <a href="#" class="list-group-item">Lab 8</a>
+            <a href="#" class="list-group-item">Lab 9</a>
+            <a href="#" class="list-group-item">Lab 10</a>
+          </div>
+        </div><!--/.sidebar-offcanvas-->
+      </div><!--/row-->
 
-		?>
-	</p>
+      <hr>
 
-</body>
-</hmtl>
+  <?php include('footer.php'); ?>
