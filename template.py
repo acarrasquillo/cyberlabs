0e07fef743f495e60f45b73149f885c5e1d554a1
torombolo17 <j.r.delavega17@gmail.com>
2015-03-03 17:22:46 -0400

    started the index.cgi and fixed the template

--- template.py
+++ template.py
@@ -39,7 +39,7 @@ def navbar():
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="index.php">Cybersecurity Labs</a>
+          <a class="navbar-brand" href="index.cgi">Cybersecurity Labs</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
